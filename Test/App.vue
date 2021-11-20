f4a174c7cb51accbdde69e41639dc423bd4519d6
shyheng <shy2210278285@aliyun.com>
2021-11-20 10:14:35 +0800

    测试

---
+++ Test/App.vue
@@ -0,0 +1,19 @@
+<script>
+	export default {
+		onLaunch: function() {
+			// 第一次运行
+			console.log('App Launch')
+		},
+		onShow: function() {
+			// 界面显示
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
